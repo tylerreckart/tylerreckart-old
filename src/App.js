c06056544821ca82566f90382ef91ad55bf6146b
Tyler Reckart <tyler.reckart@gmail.com>
2017-04-06 12:51:44 -0400

    init firebase app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react';
+import firebase, { initializeApp } from 'firebase';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+initializeApp(config);
+// console.log(firebase.app().name);
 
 class App extends Component {
   render() {
