1398dfec08fb55a9f14751e7382cae05d79f75b2
Tyler Reckart <tyler.reckart@gmail.com>
2017-05-03 15:14:49 -0400

    Convert markdown to HTML

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { css, StyleSheet } from 'aphrodite';
+import { Converter } from 'showdown';
 import { formatDate, readingTime, summarize } from '../utils/componentUtils';
 
 const Post = (props) => {
@@ -18,6 +19,7 @@ const Post = (props) => {
       display: 'block',
       fontSize: '16px',
       fontWeight: '600',
+      margin: '.75em 0',
       maxWidth: '650px',
     },
     permalink: {
@@ -31,7 +33,7 @@ const Post = (props) => {
     },
     summary: {
       fontSize: '16px',
-      lineHeight: '1.5em',
+      lineHeight: '1.75em',
       maxWidth: '650px',
     },
     title: {
@@ -39,24 +41,25 @@ const Post = (props) => {
       fontSize: '28px',
       fontWeight: '800',
       letterSpacing: '0.05em',
-      margin: '.5em 0',
       maxWidth: '650px',
     },
   });
 
+  const body = new Converter().makeHtml(content);
+
   return (
     <div className={className}>
       <div>
-        <span className={css(Styles.meta)}>
-          {formatDate(datePublished)}
-        </span>
-
         <h2 className={css(Styles.title)}>
           <a className={css(Styles.permalink)} href={url}>{title}</a>
         </h2>
 
+        <span className={css(Styles.meta)}>
+          {formatDate(datePublished)} {readingTime(content)}
+        </span>
+
         <p className={css(Styles.summary)}>
-          {!summary ? content : summarize(content)}
+          {!summary ? <div dangerouslySetInnerHTML={{ __html: body }} /> : summarize(content)}
         </p>
         {!summary ? <div /> : <a className={css(Styles.permalink, Styles.readMore)} href={url}>Read More</a>}
       </div>
