33a82dc9dcf7314696441480db59e129efcd6a92
Tyler Reckart <tyler.reckart@gmail.com>
2017-11-09 11:28:55 -0500

    Moves DB to AWS, updates server logic

--- capybara/server/index.js
+++ capybara/server/index.js
@@ -1,5 +1,8 @@
+// Declare this right away so that we know what kind of
+// enviornment we're working in
 process.env.NODE_ENV = 'development';
 
+// Dependencies
 import express from 'express';
 import subdomain from 'express-subdomain';
 import postgraphql from 'postgraphql';
@@ -7,31 +10,42 @@ import http from 'http';
 import https from 'https';
 import fs from 'fs';
 import cors from 'cors';
-
+// Make sure that we can read external environment variables
 require('dotenv').config();
 
 const app = express();
 const router = express.Router();
 
-const credentials = {
-  // key: fs.readFileSync('', 'utf8'),
-  // cert: fs.readFileSync('', 'utf8'),
-};
+// const credentials = {
+//   key: fs.readFileSync(process.env.SSL_KEY_PATH, 'utf8'),
+//   cert: fs.readFileSync(process.env.SSL_CERT_PATH, 'utf8'),
+// };
 
+// Declare subdomain
 app.use(subdomain('api', router));
-
-router.get('/', (req, res) => {
-  res.send('Welcome to the API');
-});
-
+// Connect to PSQL
 app.use(cors(), postgraphql(
-  'postgres://localhost:5432/treckart',
+  process.env.DATABASE_URL,
   'api',
   { graphiql: true }
 ));
 
+const PORT = 8080;
+
 if (process.env.NODE_ENV !== '"production"') {
-  http.createServer(app).listen(8080);
+  http.createServer(app).listen(PORT, () => (
+    console.log(`
+      POST endpoint available at http://api.${process.env.SITE_URL}/graphql
+
+      GraphiQL IDE available at http://api.${process.env.SITE_URL}/graphiql
+    `)
+  ));
 } else {
-  https.createServer(credentials, app).listen(8080);
+  https.createServer(credentials, app).listen(PORT, () => (
+    console.log(`
+      POST endpoint available at https://api.${process.env.SITE_URL}/graphql
+
+      GraphiQL IDE available at https://api.${process.env.SITE_URL}/graphiql
+    `)
+  ));
 }
